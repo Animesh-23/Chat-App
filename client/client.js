9c1d029599625b6d65d950f7dc495db0f6e14048
Animesh <animeshyesugade2002@gmail.com>
2023-08-28 16:11:56 +0530

    worked on user validation

--- client/client.js
+++ client/client.js
@@ -1,23 +1,30 @@
-// Create WebSocket connection.
 const socket = new WebSocket("ws://localhost:8080");
-const inputMsg = document.getElementById("input");
-const outputMsg = document.getElementById("output");
-const sendBtn = document.getElementById("send");
-socket.addEventListener("open", (event) => {
-  socket.send("Hello Server!");
-});
-const printMessage = (msg) => {
-  const span = document.createElement("span");
-  span.textContent = msg;
-  outputMsg.appendChild(span);
-};
-const sendMessage = () => {
-  const msg = inputMsg.value;
-  socket.send(msg);
-};
-socket.addEventListener("message", (event) => {
-  console.log(event);
-  printMessage(event.data);
-});
 
-sendBtn.addEventListener("click", sendMessage);
+const enterForm = document.getElementById("enter");
+
+socket.addEventListener("open", (event) => {
+  console.log(event);
+  console.log("Coneected to server");
+});
+enterForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  socket.username = event.target.username.value;
+  socket.send("Hello Server ");
+});
+// const printMessage = (msg) => {
+//   const span = document.createElement("span");
+//   span.textContent = msg;
+//   outputMsg.appendChild(span);
+// };
+
+// const sendMessage = () => {
+//   const msg = inputMsg.value;
+//   socket.send(msg);
+// };
+
+// socket.addEventListener("message", (event) => {
+//   console.log(event);
+//   printMessage(event.data);
+// });
+
+// sendBtn.addEventListener("click", sendMessage);
--- server/server.js
+++ server/server.js
@@ -3,10 +3,16 @@ import WebSocket, { WebSocketServer as Server } from "ws";
 const wss = new Server({ port: 8080 });
 
 const clients = new Set();
-
+const usernames = new Set();
 wss.on("connection", (ws) => {
+  console.log("client connected " + ws.username);
   clients.add(ws);
-
+  if (usernames.has(ws.username)) {
+    console.log("Username is already taken");
+    ws.close();
+  }
+  usernames.add(ws.username);
+  console.log(usernames);
   ws.on("message", (message) => {
     wss.clients.forEach((client) => {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
